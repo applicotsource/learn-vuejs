67c243fde6882648c9a6ef411507914f9134dddb
nanotumblr <peropoyi@gmail.com>
2020-01-26 11:46:53 +0900

    feat: display calendar

---
+++ step-2/components/Calendar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="calendar">
+    <calendar-day v-for="day in dayList" :key="day" :day="day" />
+  </div>
+</template>
+
+<script>
+import CalendarDay from '~/components/CalendarDay'
+export default {
+  components: {
+    CalendarDay,
+  },
+  data() {
+    return {
+      dayList: [...Array(31).keys()].map(i => ++i) // [1, 2, 3, 4,....,60]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.calendar {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
---
+++ step-2/components/CalendarDay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="calendar-day">
+    {{ day }}
+  </div>
+</template>
+
+<style scoped>
+.calendar-day {
+  border: solid 1px black;
+  width: 150px;
+  height: 150px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    day: 0,
+  }
+}
+</script>
